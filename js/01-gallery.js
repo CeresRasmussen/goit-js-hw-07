1cd4bf8b0e74460e9cda3366c65047e3d0aa9f62
CeresRasmussen <112823848+CeresRasmussen@users.noreply.github.com>
2023-02-10 22:11:43 +0200

    gallery done

---
+++ js/01-gallery.js
@@ -0,0 +1,46 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+const gallery = document.querySelector(".gallery");
+
+const galleryList = galleryItems.reduce(
+  (prew, next) =>
+    prew +
+    `<div class="gallery__item">
+  <a class="gallery__link" href="${next.original}">
+    <img
+      class="gallery__image"
+      src="${next.preview}"
+      data-source="${next.original}"
+      alt="Image description"
+    />
+  </a>
+</div>`,
+  ""
+);
+
+gallery.insertAdjacentHTML("afterbegin", galleryList);
+
+const openModalPicture = (e) => {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") return;
+  const instance = basicLightbox.create(
+    `
+      <img src="${e.target.dataset.source}" width="800" height="600">
+  `,
+    {
+      onShow: () => document.body.addEventListener("keydown", closeOnESC),
+      onclose: () => document.body.removeEventListener("keydown", closeOnESC),
+    }
+  );
+
+  const closeOnESC = (e) => {
+    if (e.code === "Escape") {
+      instance.close();
+    }
+  };
+
+  instance.show();
+  ``;
+};
+
+gallery.addEventListener("click", openModalPicture);
